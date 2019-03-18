6af4ccfaa7c8e6a643330a1e679a59cdf8d8cd4c
ousttrue <ousttrue@gmail.com>
2019-03-19 04:10:06 +0900

    Separate RectRegion.cs

---
+++ RectUI/RectRegion.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+
+namespace RectUI
+{
+    public class RectRegion
+    {
+        public virtual IEnumerable<DrawInfo> Traverse()
+        {
+            yield return _drawInfo;
+        }
+
+        DrawInfo _drawInfo = new DrawInfo();
+
+        #region Rect
+        public virtual Rect Rect
+        {
+            get { return _drawInfo.Rect; }
+            set { _drawInfo.Rect = value; }
+        }
+
+        public RectRegion()
+        { }
+
+        public RectRegion(int x, int y, int w, int h)
+        {
+            Rect = new Rect(x, y, w, h);
+        }
+        #endregion
+
+        #region Mouse
+        public virtual void MouseLeftDown()
+        {
+            if (_drawInfo.IsMouseInclude)
+            {
+                _drawInfo.MouseLeftDown = true;
+            }
+        }
+        public virtual void MouseLeftUp()
+        {
+            _drawInfo.MouseLeftDown = false;
+        }
+        public virtual void MouseRightDown()
+        {
+            _drawInfo.MouseRightDown = true;
+        }
+        public virtual void MouseRightUp()
+        {
+            _drawInfo.MouseRightDown = false;
+        }
+        public virtual void MouseMiddleDown()
+        {
+            _drawInfo.MouseMiddleDown = true;
+        }
+        public virtual void MouseMiddleUp()
+        {
+            _drawInfo.MouseMiddleDown = false;
+        }
+
+        public virtual void MouseMove(int x, int y)
+        {
+            _drawInfo.MouseMove(x, y);
+        }
+        #endregion
+    }
+}
