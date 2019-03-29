28f67bf4d2e17eef94d89a73eda180dd5c171c4b
ousttrue <ousttrue@gmail.com>
2019-03-30 04:15:02 +0900

    Separated FileDialog

---
+++ RectUI/Widgets/FileDialog.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+
+namespace RectUI.Widgets
+{
+    public class FileDialog
+    {
+        public RectRegion UI;
+
+        public event Action<FileSystemInfo> FileChanged;
+
+        public class AwaiterContext
+        {
+            public Awaiter Awaiter;
+            public Action Complete;
+            public Action<FileInfo> SetResult;
+        }
+        AwaiterContext m_context;
+
+        /// <summary>
+        /// Call when dialog window is closed
+        /// </summary>
+        public void Close()
+        {
+            var context = m_context;
+            m_context = null;
+            context.Complete();
+        }
+
+        public FileDialog(string openDir)
+        {
+            FileChanged += (obj) =>
+            {
+                var f = obj as FileInfo;
+                if (f != null)
+                {
+                    m_context.SetResult(f);
+                }
+            };
+
+            var source = new DirSource(Path.GetDirectoryName(openDir));
+
+            source.Entered += obj =>
+            {
+                FileChanged?.Invoke(obj);
+            };
+
+            UI = new VBoxRegion()
+            {
+                new ListRegion<FileSystemInfo>(source)
+                {
+                    BoxItem = BoxItem.Expand,
+                },
+
+                new HBoxRegion(new Rect(200, 40))
+                {
+                    new ButtonRegion(_ =>
+                    {
+
+                    })
+                    {
+                        Content = "Open",
+                        Rect = new Rect(96, 24),
+                        Anchor = new Anchor
+                        {
+                            Bottom=5, Left=5,
+                        }
+                    },
+
+                    new ButtonRegion(_ =>
+                    {
+
+                    })
+                    {
+                        Content = "Cancel",
+                        Rect = new Rect(96, 24),
+                        Anchor = new Anchor
+                        {
+                            Bottom=5, Right=5,
+                        }
+                    }
+                }
+            };
+        }
+
+        public event Action BeginDialogOpen;
+
+        public Awaitable OpenAsync()
+        {
+            BeginDialogOpen?.Invoke();
+
+            if (m_context != null)
+            {
+                throw new Exception();
+            }
+
+            m_context = Awaiter.Create();
+
+            return new Awaitable(m_context.Awaiter);
+        }
+
+        public class Awaitable
+        {
+            Awaiter m_awaiter;
+
+            public Awaitable(Awaiter awaiter)
+            {
+                m_awaiter = awaiter;
+            }
+
+            public Awaiter GetAwaiter()
+            {
+                return m_awaiter;
+            }
+        }
+
+        public class Awaiter : INotifyCompletion
+        {
+            FileSystemInfo m_result;
+            bool m_isCompleted;
+
+            Awaiter() { }
+
+            public static AwaiterContext Create()
+            {
+                var awaiter = new Awaiter();
+                return new AwaiterContext
+                {
+                    Awaiter = awaiter,
+                    Complete = () =>
+                    {
+                        awaiter.m_isCompleted = true;
+                        awaiter.m_continuation();
+                    },
+                    SetResult = f => awaiter.m_result = f,
+                };
+            }
+
+            public bool IsCompleted => m_isCompleted;
+
+            Action m_continuation;
+            public void OnCompleted(Action continuation)
+            {
+                m_continuation = continuation;
+            }
+
+            public FileSystemInfo GetResult()
+            {
+                return m_result;
+            }
+        }
+    }
+}
--- RectUIGLTFSample/Program.cs
+++ RectUIGLTFSample/Program.cs
@@ -4,162 +4,14 @@ using RectUI.Assets;
 using RectUI.Widgets;
 using System;
 using System.IO;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 
 namespace RectUIGLTFSample
 {
-    class FileDialog
-    {
-        public RectRegion UI;
-        public Window Window;
-
-        public event Action<FileSystemInfo> FileChanged;
-
-        public class AwaiterContext
-        {
-            public Awaiter Awaiter;
-            public Action Complete;
-            public Action<FileInfo> SetResult;
-        }
-        AwaiterContext m_context;
-
-        public FileDialog(Window parent, string openDir)
-        {
-            Window = parent.CreateModal(400, 300);
-            Window.OnClose += () =>
-            {
-                var context = m_context;
-                m_context = null;
-                context.Complete();
-            };
-
-            FileChanged += (obj) =>
-            {
-                var f = obj as FileInfo;
-                if (f != null)
-                {
-                    m_context.SetResult(f);
-                    parent.Enable();
-                    Window.Close();
-                }
-            };
-
-            var source = new DirSource(Path.GetDirectoryName(openDir));
-
-            source.Entered += obj =>
-            {
-                FileChanged?.Invoke(obj);
-            };
-
-            UI = new VBoxRegion()
-            {
-                new ListRegion<FileSystemInfo>(source)
-                {
-                    BoxItem = BoxItem.Expand,
-                },
-
-                new HBoxRegion(new Rect(200, 40))
-                {
-                    new ButtonRegion(_ =>
-                    {
-
-                    })
-                    {
-                        Content = "Open",
-                        Rect = new Rect(96, 24),
-                        Anchor = new Anchor
-                        {
-                            Bottom=5, Left=5,
-                        }
-                    },
-
-                    new ButtonRegion(_ =>
-                    {
-
-                    })
-                    {
-                        Content = "Cancel",
-                        Rect = new Rect(96, 24),
-                        Anchor = new Anchor
-                        {
-                            Bottom=5, Right=5,
-                        }
-                    }
-                }
-            };
-        }
-
-        public Awaitable OpenAsync()
-        {
-            Window.Show(SW.SHOW);
-
-            if (m_context != null)
-            {
-                throw new Exception();
-            }
-
-            m_context = Awaiter.Create();
-
-            return new Awaitable(m_context.Awaiter);
-        }
-
-        public class Awaitable
-        {
-            Awaiter m_awaiter;
-
-            public Awaitable(Awaiter awaiter)
-            {
-                m_awaiter = awaiter;
-            }
-
-            public Awaiter GetAwaiter()
-            {
-                return m_awaiter;
-            }
-        }
-
-        public class Awaiter : INotifyCompletion
-        {
-            FileSystemInfo m_result;
-            bool m_isCompleted;
-
-            Awaiter() { }
-
-            public static AwaiterContext Create()
-            {
-                var awaiter = new Awaiter();
-                return new AwaiterContext
-                {
-                    Awaiter = awaiter,
-                    Complete = () =>
-                    {
-                        awaiter.m_isCompleted = true;
-                        awaiter.m_continuation();
-                    },
-                    SetResult = f => awaiter.m_result = f,
-                };
-            }
-
-            public bool IsCompleted => m_isCompleted;
-
-            Action m_continuation;
-            public void OnCompleted(Action continuation)
-            {
-                m_continuation = continuation;
-            }
-
-            public FileSystemInfo GetResult()
-            {
-                return m_result;
-            }
-        }
-    }
-
     class Program
     {
-        static RectRegion BuildUI(Window dialog,
+        static RectRegion BuildUI(
             Scene scene,
             Action onOpen)
         {
@@ -215,22 +67,42 @@ namespace RectUIGLTFSample
             public void Run()
             {
                 var window = Window.Create(SW.SHOW);
-                var dialog = new FileDialog(window, State.Instance.OpenDir);
+                var dialogWindow = window.CreateModal(400, 300);
+
+                var dialog = new FileDialog(State.Instance.OpenDir);
+
+                dialog.BeginDialogOpen += () =>
+                {
+                    dialogWindow.Show(SW.SHOW);
+                };
+
                 dialog.FileChanged += obj =>
                 {
                     var f = obj as FileInfo;
                     if (f != null)
                     {
                         State.Instance.OpenFile = f.FullName;
+
+                        window.Enable();
+                        dialogWindow.Close();
+
+                        return;
                     }
+
                     var d = obj as DirectoryInfo;
                     if (d != null)
                     {
                         State.Instance.OpenDir = d.FullName;
+                        return;
                     }
                 };
 
-                m_app.Bind(dialog.Window, dialog.UI);
+                dialogWindow.OnClose += () =>
+                {
+                    dialog.Close();
+                };
+
+                m_app.Bind(dialogWindow, dialog.UI);
 
                 Action onOpen = async () =>
                 {
@@ -247,7 +119,7 @@ namespace RectUIGLTFSample
                         }
                     }
                 };
-                m_app.Bind(window, BuildUI(dialog.Window, m_scene, onOpen));
+                m_app.Bind(window, BuildUI(m_scene, onOpen));
 
                 Window.MessageLoop();
 
