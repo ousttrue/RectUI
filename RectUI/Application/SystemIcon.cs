a3cac441eb28ae5f9d973fc42c1e245768efeed1
ousttrue <ousttrue@gmail.com>
2019-04-01 16:29:40 +0900

    SystemIcon.cs

---
+++ RectUI/Application/SystemIcon.cs
@@ -0,0 +1,44 @@
+﻿using DesktopDll;
+using System;
+using System.Runtime.InteropServices;
+
+namespace RectUI.Application
+{
+    public class SystemIcon
+    {
+        public IntPtr ImageList
+        {
+            get;
+            private set;
+        }
+
+        public int ImageListIndex
+        {
+            get;
+            private set;
+        }
+
+        public static SystemIcon Get(string path, bool isSmall)
+        {
+            var flags = SHGFI.SYSICONINDEX;
+            if (isSmall)
+            {
+                flags |= SHGFI.SMALLICON;
+            }
+            var sfi = default(SHFILEINFOW);
+            var result = Shell32.SHGetFileInfoW(path, -1,
+                ref sfi, Marshal.SizeOf<SHFILEINFOW>(),
+                flags);
+            if (result == IntPtr.Zero)
+            {
+                return null;
+            }
+
+            return new SystemIcon
+            {
+                ImageList = result,
+                ImageListIndex = sfi.iIcon,
+            };
+        }
+    }
+}
--- RectUI/Widgets/DirSource.cs
+++ RectUI/Widgets/DirSource.cs
@@ -1,51 +1,11 @@
-﻿using DesktopDll;
-using RectUI.Graphics;
+﻿using RectUI.Graphics;
 using SharpDX;
 using System;
 using System.IO;
-using System.Runtime.InteropServices;
 
 
 namespace RectUI.Widgets
 {
-    public class SystemIcon
-    {
-        public IntPtr ImageList
-        {
-            get;
-            private set;
-        }
-
-        public int ImageListIndex
-        {
-            get;
-            private set;
-        }
-
-        public static SystemIcon Get(string path, bool isSmall)
-        {
-            var flags = SHGFI.SYSICONINDEX;
-            if (isSmall)
-            {
-                flags |= SHGFI.SMALLICON;
-            }
-            var sfi = default(SHFILEINFOW);
-            var result = Shell32.SHGetFileInfoW(path, -1,
-                ref sfi, Marshal.SizeOf<SHFILEINFOW>(),
-                flags);
-            if (result == IntPtr.Zero)
-            {
-                return null;
-            }
-
-            return new SystemIcon
-            {
-                ImageList = result,
-                ImageListIndex = sfi.iIcon,
-            };
-        }
-    }
-
     public class DirSource : ListSource<FileSystemInfo>
     {
         DirectoryInfo m_current;
